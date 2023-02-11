365777e6b7948548a5ca8f65e5486348deae3a76
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2023-02-11 17:26:42 +0100

    Add Java Spring-Boot

---
+++ src/main/java/com/my/fim/FimBackApplication.java
@@ -0,0 +1,13 @@
+package com.my.fim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/fim/FimBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.fim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FimBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
